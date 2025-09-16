dfb3180aa8443c74ab4dd1ebcf9df3f575bce2bb
Aldofrz <aldofarizfadilla@gmail.com>
2025-09-17 01:53:28 +0700

    Initial Project

---
+++ logic_test/issue.js
@@ -0,0 +1,7 @@
+function multiply(num1, num2) {
+return num1 * num2;
+}
+let a = 5;
+let b = 10;
+let multiplyResult = multiply(a, b);
+console.log("The multiply result is: " + multiplyResult);
---
+++ logic_test/star.js
@@ -0,0 +1,13 @@
+function starPattern(num) {
+    for(let i = 1; i <= num; i++) {
+        let row = ('')
+
+        for(let j = 1; j <= i; j++) {
+            row += '*'
+        }
+
+        console.log(row)
+    }
+}
+
+starPattern(5)
